4fc5511c330aa2936db972acc52ae3097e80e315
sina <3inroydl@gmail.com>
2024-05-11 17:37:25 +0330

    separate slider page

---
+++ app/pages/includes/slider.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="<?php echo ROOT?>/assets/slider/ism/css/my-slider.css"/>
+<script src="<?php echo ROOT?>/assets/slider/ism/js/ism-2.2.min.js"></script>
+
+<div class="ism-slider" id="my-slider">
+    <ol>
+        <li>
+            <img src="<?php echo ROOT?>/assets/slider/ism/image/slides/flower-729514_1280.jpg">
+            <div class="ism-caption ism-caption-0">My slide caption text</div>
+        </li>
+        <li>
+            <img src="<?php echo ROOT?>/assets/slider/ism/image/slides/beautiful-701678_1280.jpg">
+            <div class="ism-caption ism-caption-0">My slide caption text</div>
+        </li>
+        <li>
+            <img src="<?php echo ROOT?>/assets/slider/ism/image/slides/barley-373360_1280.jpg">
+            <div class="ism-caption ism-caption-0">My slide caption text</div>
+        </li>
+    </ol>
+</div>
+<main class="p-2">
